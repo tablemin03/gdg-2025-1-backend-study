b3a08e6165256972942f795152390d77e59a393e
tablemin03 <vs201466@naver.com>
2025-04-15 18:57:28 +0900

    week2 과제 제출

---
+++ todo-api/src/main/java/com/example/todo_api/hw/MyRepository.java
@@ -0,0 +1,11 @@
+package com.example.todo_api.hw;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRepository {
+
+    public void repositoryMethod() {
+        System.out.println("repository");
+    }
+}
